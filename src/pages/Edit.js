f2d6c6bf3dac258a2000185ecb8e918c869d0e1b
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2022-10-11 16:50:11 -0300

    Finalizando as correções de lint do requisito 1

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -4,7 +4,7 @@ class Edit extends React.Component {
   render() {
     return (
       <div data-testid="page-profile-edit">
-        <h1>Editar</h1>
+        {/* <h1>Editar</h1> */}
       </div>
     );
   }
